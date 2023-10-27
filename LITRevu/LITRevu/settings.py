31aefffdfdf64749999b5ff2652be8819fff8a1e
Matthiiews <matthieu.kouassi@gmail.com>
2023-10-27 20:13:22 +0200

    Ajout de l'APP feed

--- LITRevu/LITRevu/settings.py
+++ LITRevu/LITRevu/settings.py
@@ -127,8 +127,6 @@ STATICFILES_DIRS = [BASE_DIR.joinpath('static')]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-AUTH_USER_MODEL = 'authentication.User'
-
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'posts'
 LOGOUT_REDIRECT_URL = 'LOGIN_URL'
